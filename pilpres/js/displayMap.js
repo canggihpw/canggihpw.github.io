e9a46af6b3fb0ad69ce4e1d2e26a292b2c273cbc
canggihpw <cangcappo@gmail.com>
2018-09-23 15:56:05 +0700

    add pilpres

---
+++ pilpres/js/displayMap.js
@@ -0,0 +1,124 @@
+// Global variables declaration
+var data;
+var tempCol = [];
+var minmax;
+var q;
+
+
+// Set svg map size
+var width = 800,
+    height = 300;
+// Set svg plot size
+var dwidth = 300, 
+    dheight = 250;
+// Set padding, used for svg graph
+var padding = {
+    x: 40,
+    y: 40
+};
+
+// Map projection, viewbox focus on Indonesia
+var projection = d3.geo.mercator()
+    .scale(1000)
+    .translate([-1650,120]);
+
+// Create svgmap as a reference of svg map element
+var svgmap = d3.select("#map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Define path based on the projection
+var path = d3.geo.path()
+    .projection(projection);
+
+var g = svgmap.append("g");
+
+
+//-----------------------------
+// Load CSV for map, and quantize the data
+//-------------------------------
+d3.csv("dataset/pilpresdata.csv",function(dataloaded){
+  dataloaded.map(function(d){
+    tempCol.push(d['warna']);
+  });
+  
+  //tempCol.splice(tempCol.indexOf("-"),1);
+
+  // minmax = d3.extent(tempCol);
+  // // scale the data
+  // q = d3.scale.quantize()
+  //   .domain(minmax)
+  //   .range(colorbrewer.Reds[9]);
+
+  data = dataloaded;
+});
+//------------------------------
+//------------------------------
+
+// Define the div for the tooltip
+var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+
+//---------------------------------
+// Load JSON data to construct the map
+//-------------------------------
+d3.json("js/provinsi.json", function(error, topology) {
+    g.selectAll("path")
+      .data(topojson.feature(topology, topology.objects.map)
+          .features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("fill",function(d){
+        try{
+          return data[prov2id(d.properties.id_provinsi)]['warna'];
+        }catch{
+          return '#ffffff';
+        }
+        
+      })
+
+      .on("click",function(d){
+        // d3.select("#bigProv").html(data[prov2id(d.properties.id_provinsi)]['provinsi']);
+      })
+      .on("mousedown",function(d){
+      	d3.select(this)
+      	.style('stroke','#bbb');
+      }) 
+      .on("mouseup",function(d){
+      	d3.select(this)
+      	.style('stroke','black');
+      })     
+      .on("mouseover",function(d){
+        htmldata = data[prov2id(d.properties.id_provinsi)]['capres'] + " : " + data[prov2id(d.properties.id_provinsi)]['persen'] + " %";
+        
+        div.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            div	.html(htmldata)	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");		
+
+        d3.select(this)
+      			.style('stroke-width','2px')
+      			.style("cursor",'pointer');
+      	
+      })
+      .on("mouseout",function(d){
+      		d3.selectAll('path')
+            .style('stroke-width','1px');
+            
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+      		
+      })
+      // d3.select("#bigProv").html(data[prov2id(34)]['provinsi']);
+      // drawPlot(prov2id(34));
+      // displayRadarChart(prov2id(34));
+		
+});
+//--------------------------------------------
+//-------------------------------------------
---
+++ pilpres/js/main.js
@@ -0,0 +1,39 @@
+// Convert province ID to line number
+function prov2id(prov){
+	switch(prov){
+		case 11: return 0;break;
+		case 12: return 1;break;
+		case 13: return 2;break;
+		case 14: return 3;break;
+		case 15: return 4;break;
+		case 16: return 5;break;
+		case 17: return 6;break;
+		case 18: return 7;break;
+		case 19: return 8;break;
+		case 21: return 9;break;
+		case 31: return 10;break;
+		case 32: return 11;break;
+		case 33: return 12;break;
+		case 34: return 13;break;
+		case 35: return 14;break;
+		case 36: return 15;break;
+		case 51: return 16;break;
+		case 52: return 17;break;
+		case 53: return 18;break;
+		case 61: return 19;break;
+		case 62: return 20;break;
+		case 63: return 21;break;
+		case 64: return 22;break;
+		case 65: return 23;break;
+		case 71: return 24;break;
+		case 72: return 25;break;
+		case 73: return 26;break;
+		case 74: return 27;break;
+		case 75: return 28;break;
+		case 76: return 29;break;
+		case 81: return 30;break;
+		case 82: return 31;break;
+		case 92: return 32;break;
+		case 91: return 33;break;
+	}
+}
